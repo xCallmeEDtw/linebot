11ad42117f0f285933349d3b8193e28b5e70fd57
xCallmeEDtw <68278789+xCallmeEDtw@users.noreply.github.com>
2021-02-03 16:59:28 +0800

    up

--- Modules/OPTmessage.py
+++ Modules/OPTmessage.py
@@ -9,12 +9,17 @@ def MessageAdd(m_add,m_type):
                 "stickerId": m_add[1]
                 
                 }
+    elif m_type =='image':
+        
+        addtion={
+                "type":"text",
+                "originalContentUrl": m_add,
+                "previewImageUrl": m_add
+                }
     elif m_type =='text':
         
         addtion={
                 "type":"text",
                 "text": m_add
                 }
-
     return addtion
-# {'type': 'message', 'replyToken': '401f1beb096e4bd094fca05f1162e588', 'source': {'userId': 'Udde25b6bc63d084bbbf55c53ff0826d4', 'type': 'user'}, 'timestamp': 1612339264295, 'mode': 'active', 'message': {'type': 'sticker', 'id': '13493434162018', 'stickerId': '140', 'packageId': '2', 'stickerResourceType': 'STATIC', 'keywords': ['chase', 'play', 'skip', 'enjoy', 'Happy']}}
--- Modules/ReplyMessage.py
+++ Modules/ReplyMessage.py
@@ -16,24 +16,3 @@ def ReplyMessage(replyToken,messages):
     url = 'https://api.line.me/v2/bot/message/reply'
     r = requests.post(url,headers=headers,data=json.dumps(data))
     return r
-# def ReplyText(replyToken,text):
-#     messages = [
-#         {
-#             "type":"text",
-#             "text": text
-#         }
-#     ]
-#     ReplyMessage(replyToken,messages)
-#     return 0
-# def ReplySticker(replyToken,st_id,pk_id):
-#     messages = [
-#         {
-#             "type":"sticker",
-#             "packageId": pk_id,
-#             "stickerId": st_id
-            
-#         }
-#     ]
-#     ReplyMessage(replyToken,messages)
-#     return 0
-# {'type': 'message', 'replyToken': '401f1beb096e4bd094fca05f1162e588', 'source': {'userId': 'Udde25b6bc63d084bbbf55c53ff0826d4', 'type': 'user'}, 'timestamp': 1612339264295, 'mode': 'active', 'message': {'type': 'sticker', 'id': '13493434162018', 'stickerId': '140', 'packageId': '2', 'stickerResourceType': 'STATIC', 'keywords': ['chase', 'play', 'skip', 'enjoy', 'Happy']}}
