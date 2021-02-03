568e26a92c6943ad5ca1ca7fe53fcda8a3868966
xCallmeEDtw <68278789+xCallmeEDtw@users.noreply.github.com>
2021-02-03 15:56:21 +0800

    up

---
+++ Modules/ReplyText.py
@@ -0,0 +1,7 @@
+from ReplyMessage import ReplyMessage
+
+def ReplyText(replyToken,text):
+    #請使用自己的token
+
+
+ReplyMessage(replyToken,messages)
--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from flask import Flask,request
-from Modules.ReplyMessage import ReplyMessage
+from Modules.ReplyMessage import ReplyText
 app = Flask(__name__)
 #HTTP請求方式
 @app.route('/', methods=['GET', 'POST'])
-# def myreply(replyToken , text):
-# 		messages = [
-# 		    {
-# 		        "type":"text",
-# 		        "text": text
-# 		    }
-# 		]
-
-# 		return ReplyMessage(replyToken,messages)
-
 
 def index():
 	if request.method == 'POST':
@@ -25,14 +15,7 @@ def index():
 # {'type': 'message', 'replyToken': '4128834b76bc44159cbc7da6c3b6e36a', 'source': {'userId': 'Udde25b6bc63d084bbbf55c53ff0826d4', 'type': 'user'}, 'timestamp': 1612336487515, 'mode': 'active', 'message': {'type': 'text', 'id': '13493228092527', 'text': '有'}}
 		# if message.
 		if text == 'ssd':
-			# messages = [
-			#     {
-			#         "type":"text",
-			#         "text": 
-			#     }
-			# ]
-			ReplyMessage(replyToken,"go buy ssd la!")
-
+			ReplyText(replyToken,"go buy ssd la!")
 
 		return 'SUCCEED'
 	elif request.method == 'GET':
