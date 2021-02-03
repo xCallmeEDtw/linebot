945734b581ddb6cc6f7058014a9e93544706427d
xCallmeEDtw <68278789+xCallmeEDtw@users.noreply.github.com>
2021-02-03 14:23:22 +0800

    up

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 #HTTP請求方式
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    return 'buy ssd la!'
-
+	if request.method == 'POST':
+		return 'buy ssd la'
+	elif request.method == 'GET':
+		return 'buy ssd b***ch'
+	else:
+		return 'tell u to buy ssd'
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=5000,debug=True, use_reloader=False)
 
