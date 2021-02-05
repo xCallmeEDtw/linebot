909e90e7792070d5abe8855f66caec769c68617c
xCallmeEDtw <68278789+xCallmeEDtw@users.noreply.github.com>
2021-02-05 11:58:14 +0800

    Update FlexEdit.py

--- Modules/FlexEdit.py
+++ Modules/FlexEdit.py
@@ -1,4 +1,5 @@
-def FlexEdit(): #call
+def FlexEdit(text): #call
+	
 	flexMessage1 = {
 	  "type": "bubble",
 	  "hero": {
@@ -155,72 +156,72 @@ def FlexEdit(): #call
 	      }
 	    ]
 	  }
+
 	}
 
-
-	# flexMessage2 = {
-	#   "type": "bubble",
-	#   "direction": "ltr",
-	#   "header": {
-	#     "type": "box",
-	#     "layout": "horizontal",
-	#     "backgroundColor": "#0E0D0DFF",
-	#     "contents": [
-	#       {
-	#         "type": "text",
-	#         "text": "KALI",
-	#         "size": "4xl",
-	#         "color": "#FFFEFFFF",
-	#         "align": "center",
-	#         "contents": []
-	#       }
-	#     ]
-	#   },
-	#   "hero": {
-	#     "type": "image",
-	#     "url": "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6r1hk1EunQirF1IcY4TG8T/fa1e41a93eb39fb33acb3df101f5e748/r6-operators-badge-kali_358313.png",
-	#     "size": "full",
-	#     "aspectRatio": "1.51:1",
-	#     "aspectMode": "fit"
-	#   },
-	#   "body": {
-	#     "type": "box",
-	#     "layout": "vertical",
-	#     "contents": [
-	#       {
-	#         "type": "text",
-	#         "text": "獨特技能與遊玩風格 想要談論低速爆破矛，就必須先提一提 Kali 的 CSRX 300。這是種高危險性武器，能一擊摧毀封阻物和艙板，並在可破壞牆面上開個洞。低速爆破矛是其槍管下掛配件，能與 CSRX 300 的瞄準鏡同時使用。 它的作用是在一定範圍內透過輕微爆炸來破壞裝備。對可破壞牆面使用時，它會形成一個比狙擊步槍槍孔略小的洞。而對強化牆面射擊時，它只會附著在表面。",
-	#         "weight": "bold",
-	#         "size": "lg",
-	#         "align": "center",
-	#         "wrap": True,
-	#         "contents": []
-	#       }
-	#     ]
-	#   },
-	#   "footer": {
-	#     "type": "box",
-	#     "layout": "horizontal",
-	#     "contents": [
-	#       {
-	#         "type": "button",
-	#         "action": {
-	#           "type": "uri",
-	#           "label": "官方網站",
-	#           "uri": "https://www.ubisoft.com/zh-tw/game/rainbow-six/siege/game-info/operators/kali"
-	#         },
-	#         "color": "#EDECC8FF",
-	#         "margin": "xs",
-	#         "height": "sm",
-	#         "style": "link"
-	#       }
-	#     ]
-	#   }
-	# }
-	# if text == 1:
-	# 	return flexMessage1
-	# elif text == 2:
-	# 	return flexMessage2
+	flexMessage2 ={
+	  "type": "bubble",
+	  "direction": "ltr",
+	  "header": {
+	    "type": "box",
+	    "layout": "horizontal",
+	    "backgroundColor": "#0E0D0DFF",
+	    "contents": [
+	      {
+	        "type": "text",
+	        "text": "KALI",
+	        "size": "4xl",
+	        "color": "#FFFEFFFF",
+	        "align": "center",
+	        "contents": []
+	      }
+	    ]
+	  },
+	  "hero": {
+	    "type": "image",
+	    "url": "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6r1hk1EunQirF1IcY4TG8T/fa1e41a93eb39fb33acb3df101f5e748/r6-operators-badge-kali_358313.png",
+	    "size": "full",
+	    "aspectRatio": "1.51:1",
+	    "aspectMode": "fit"
+	  },
+	  "body": {
+	    "type": "box",
+	    "layout": "vertical",
+	    "contents": [
+	      {
+	        "type": "text",
+	        "text": "獨特技能與遊玩風格 想要談論低速爆破矛，就必須先提一提 Kali 的 CSRX 300。這是種高危險性武器，能一擊摧毀封阻物和艙板，並在可破壞牆面上開個洞。低速爆破矛是其槍管下掛配件，能與 CSRX 300 的瞄準鏡同時使用。 它的作用是在一定範圍內透過輕微爆炸來破壞裝備。對可破壞牆面使用時，它會形成一個比狙擊步槍槍孔略小的洞。而對強化牆面射擊時，它只會附著在表面。",
+	        "weight": "bold",
+	        "size": "lg",
+	        "align": "center",
+	        "wrap": true,
+	        "contents": []
+	      }
+	    ]
+	  },
+	  "footer": {
+	    "type": "box",
+	    "layout": "horizontal",
+	    "contents": [
+	      {
+	        "type": "button",
+	        "action": {
+	          "type": "uri",
+	          "label": "官方網站",
+	          "uri": "https://www.ubisoft.com/zh-tw/game/rainbow-six/siege/game-info/operators/kali"
+	        },
+	        "color": "#EDECC8FF",
+	        "margin": "xs",
+	        "height": "sm",
+	        "style": "link"
+	      }
+	    ]
+	  }
+	}
+	if text == 1:
+		return flexMessage1
+	elif text == 2:
+		return flexMessage2
 
 
 
