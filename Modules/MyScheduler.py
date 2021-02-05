0674bc5c8da23bb3bd6d30410a5361fac319f857
xCallmeEDtw <68278789+xCallmeEDtw@users.noreply.github.com>
2021-02-05 14:05:43 +0800

    Update MyScheduler.py

--- Modules/MyScheduler.py
+++ Modules/MyScheduler.py
@@ -7,7 +7,9 @@ from PushMessage import PushMessage
 def sensor():
     url= 'https://gobuyssd.herokuapp.com/'
     r = requests.get(url)
-    print(r.text)  
+    print(r.status_code)
+    print(r.text) 
+
     
 # def work1():
 #     r = requests.get('https://gobuyssd.herokuapp.com/')
@@ -20,6 +22,7 @@ def MyScheduler():
         start_date='2019-4-24 08:00:00',
         end_date='2099-4-24 08:00:00',
         timezone='Asia/Taipei')
+
     sched.add_job(sensor,trigger=interval)
 
     sched.start()
