9efe7cef3803d5e55c1859455035d9ed26e7fa1b
xCallmeEDtw <68278789+xCallmeEDtw@users.noreply.github.com>
2021-02-05 14:42:18 +0800

    123

--- Modules/MyScheduler2.py
+++ Modules/MyScheduler2.py
@@ -2,13 +2,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 import requests
 import os
-
+import atexit
 def sensor():
-    print('123')
-    url= 'https://gobuyssd.herokuapp.com/alive'
+    url="https://gobuyssd.herokuapp.com/"
     r = requests.get(url)
     print(r.status_code)
-    #print(r.text) 
+    print(r.text)  
+
 
     
 # def work1():
@@ -18,14 +18,10 @@ def sensor():
 def MyScheduler():
     sched = BackgroundScheduler(daemon=True)
     interval = IntervalTrigger(
-        minutes= 1,
+        minutes = 1,
         start_date='2019-4-24 08:00:00',
         end_date='2099-4-24 08:00:00',
-        timezone='Asia/Taipei')
+        timezone='Asia/Shanghai')
     sched.add_job(sensor,trigger=interval)
-    sched.add_job(work1, 'cron', hour=12, minute=30, end_date='2021-10-30',timezone='Asia/Taipei')
-    sched.add_job(work1, 'cron', hour=19, minute=00, end_date='2021-10-30',timezone='Asia/Taipei')
-    
-
     sched.start()
-    return ''
+    atexit.register(lambda: sched.shutdown())
--- main.py
+++ main.py
@@ -10,12 +10,13 @@ from Modules.BigData import RandomPicture
 from Modules.FlexEdit import FlexEdit
 from Modules.MyScheduler import MyScheduler
 
-MyScheduler()
+
 # import random
 app = Flask(__name__) #初始化
 #HTTP請求方式
 @app.route('/', methods=['GET', 'POST']) #根目錄
 # {'type': 'message', 'replyToken': '401f1beb096e4bd094fca05f1162e588', 'source': {'userId': 'Udde25b6bc63d084bbbf55c53ff0826d4', 'type': 'user'}, 'timestamp': 1612339264295, 'mode': 'active', 'message': {'type': 'sticker', 'id': '13493434162018', 'stickerId': '140', 'packageId': '2', 'stickerResourceType': 'STATIC', 'keywords': ['chase', 'play', 'skip', 'enjoy', 'Happy']}}
+
 def index():
 	con = ['剪刀' ,'石頭','布']
 	horos = ["水瓶",'雙魚','牡羊','金牛','雙子','巨蟹','獅子','處女','天平','天蠍','射手','魔羯']
